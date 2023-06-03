4351cd730e15adac94f8c5068172ca08c687c544
Adrian Coria <adrian.c.coria@gmail.com>
2023-06-02 22:05:33 -0700

    add clear button

--- eas.js
+++ eas.js
@@ -1,12 +1,29 @@
 const gridSizeInput = document.getElementById('grid-size');
 const gridSizeValue = document.getElementById('grid-size-value');
 const gridContainer = document.getElementById('grid-container');
+const randomColorsCheckbox = document.getElementById('random-colors-checkbox');
+const plainBlackCheckbox = document.getElementById('plain-black-checkbox');
+const clearButton = document.getElementById('clear-button');
 let size = gridSizeInput.value;
+let useRandomColors = false;
+let usePlainBlack = true;
 
 console.log("Hello World");
 // initial grid
 generateGrid(16);
 addGridItemListeners();
+
+// Handle random colors checkbox change
+randomColorsCheckbox.addEventListener('change', (e) => {
+    useRandomColors = e.target.checked;
+});
+
+plainBlackCheckbox.addEventListener('change', (e) => {
+    usePlainBlack = e.target.checked;
+});
+
+// Handle clear button
+clearButton.addEventListener('click', clearGridColors);
 
 // update grid size when slider value changes
 gridSizeInput.addEventListener('input',(e) => {
@@ -35,8 +52,6 @@ function clearGrid() {
     }
 }
 
-const gridItems = document.querySelectorAll('.grid-item');
-
 function addGridItemListeners() {
     const gridItems = document.querySelectorAll('.grid-item');
     gridItems.forEach((gridItem) => {
@@ -46,8 +61,15 @@ function addGridItemListeners() {
 
 function changeColor(e) {
     const gridItem = e.target;
-    const randomColor = getRandomColor();
-    gridItem.style.backgroundColor = randomColor;
+    
+    if(useRandomColors) {
+        const randomColor = getRandomColor();
+        gridItem.style.backgroundColor = randomColor;
+    } else if (usePlainBlack) {
+        gridItem.style.backgroundColor = 'black';
+    } else {
+        gridItem.style.backgroundColor = '#9fc2b2';
+    }
 }
 
 function getRandomColor() {
@@ -57,6 +79,13 @@ function getRandomColor() {
         color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
+}
+
+function clearGridColors() {
+    const gridItems = document.querySelectorAll('.grid-item');
+    gridItems.forEach((gridItem) => {
+        gridItem.style.backgroundColor = '#9fc2b2';
+    });
 }
 
 
